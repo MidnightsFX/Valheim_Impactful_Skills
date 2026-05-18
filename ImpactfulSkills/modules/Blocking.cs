b6f0b038bd657e382d0556abd3b9126f87a615d5
Lesliechan201@gmail.com <Lesliechan201@gmail.com>
2026-05-17 21:47:17 -0400

    Added block stamina gain, and additional block power at configurable thresholds. Its in the configs!
    Added Equip and Unequip speed scalable modifier to all WeaponSkills.

---
+++ ImpactfulSkills/modules/Blocking.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ImpactfulSkills.patches
+{
+    public static class Blocking
+    {
+        [HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetBlockPower), typeof(int), typeof(float))]
+        internal class BlockPower_Patch
+        { 
+            [HarmonyPostfix]
+            static void Postfix(float skillFactor, ref float __result)
+            {
+                if (ValConfig.EnableBlocking.Value != true || Player.m_localPlayer == null) { return; }
+
+                if (Player.m_localPlayer.GetSkillLevel(Skills.SkillType.Blocking) >= ValConfig.BlockPowerRequiredLevel.Value)
+                {
+                    float baseBlock = __result / (1f + (skillFactor) * 0.5f);
+                    __result = baseBlock + baseBlock * (skillFactor) * ValConfig.BlockPowerFactor.Value; // its configuraable!
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.BlockAttack))]
+        internal class BlockStaminaGain_Patch
+        {
+            [HarmonyPostfix]
+            static void Postfix(Humanoid __instance, bool __result)
+            {
+                if (ValConfig.EnableBlocking.Value != true || Player.m_localPlayer == null || (!__result) || __instance != Player.m_localPlayer) { return; }
+
+                float skillLevel = Player.m_localPlayer.GetSkillLevel(Skills.SkillType.Blocking);
+
+                if (skillLevel >= ValConfig.BlockStaminaGainRequiredLevel.Value)
+                {
+                    float staminaGain = (skillLevel / (ValConfig.BlockStaminaGainFactor.Value * 100)) * __instance.m_blockStaminaDrain;
+                    __instance.AddStamina(staminaGain);
+                }
+            }
+        }
+    }
+}
